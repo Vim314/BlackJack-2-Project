863a9b8b914ffa58797d27d60a4d36ecbe2ff2b0
Vim314 <47543571+Vim314@users.noreply.github.com>
2022-10-18 19:33:24 -0400

    Finished card class

--- deck.py
+++ deck.py
@@ -11,11 +11,23 @@ import numpy as np
 
 #Creating Individual Card Class
 class Card:
-    def __init__(self, value, suit):
+    def __init__(self, value, suit, fullDeck):
         self.value = value
         self.suit = suit
-    
-    value = np.array(["ace", 2, 3, 4, 5, 6, 7, 8, 9, 10, "jack", "queen", "king"])
-    suit = np.array(["clubs", "hearts", "diamonds", "spades"])
+        self.fullDeck = fullDeck
 
-    print(value[3])
+        value = np.array(["ace", 2, 3, 4, 5, 6, 7, 8, 9, 10, "jack", "queen", "king"])
+        suit = np.array(["clubs", "hearts", "diamonds", "spades"])
+
+        fullDeck = np.array(np.meshgrid(value, suit)).T.reshape(52,-1)
+
+    def shuffle(self):
+        np.random.shuffle(self.fullDeck)
+
+    def deal(self):
+        card = self.fullDeck.pop()
+        return card
+
+
+    
+    
